2a6d2548772a71a40ad9168129d9bd8f0f64942b
Olga Dolinher <olga_dolinher@epam.com>
2023-03-09 23:38:59 +0000

    L3: setup done, Users table created

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+import { createSqlServer } from './src/L3/index';
 import createServer from "./src/L2/lecture2"
 
-createServer()
+// Lecture 2
+//  createServer()
+
+
+// Lecture 3
+createSqlServer()
